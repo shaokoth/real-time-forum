c5880f1d309481f8ce2a3fefe7d377bea72341ca
shaokoth <shadrackomondi005@gmail.com>
2025-05-09 11:59:20 +0300

    feat: Add  function to create users table

---
+++ backend/users.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// ==== The function will create the users table in the database =====
+func CreateUsersTable(db *sql.DB) error {
+	query := `
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        uuid TEXT UNIQUE NOT NULL,
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    );`
+	if _, err := db.Exec(query); err != nil {
+		return err
+	}
+	fmt.Println("[SUCCESS]: Created the users table")
+	return nil
+}
