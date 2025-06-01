f1bb564c0fe822f2f78369da4b3cd627b1c58e58
hilaromondi <omondihilary19@gmail.com>
2025-06-01 23:19:08 +0300

    chore: remove space

--- backend/database/users.go
+++ backend/database/users.go
@@ -10,20 +10,20 @@ import (
 // ==== The function will create the users table in the database =====
 func CreateUsersTable(db *sql.DB) error {
 	query := `
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        uuid TEXT UNIQUE NOT NULL,
-        nickname TEXT UNIQUE NOT NULL,
-		age INTEGER  NOT NULL,
-		gender TEXT NOT NULL,
-		first_name TEXT  UNIQUE NOT NULL,
-		last_name  TEXT  NOT NULL,
-        email TEXT UNIQUE NOT NULL,
-        password TEXT NOT NULL
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uuid TEXT UNIQUE NOT NULL,
+    nickname TEXT UNIQUE NOT NULL,
+    age INTEGER NOT NULL,
+    gender TEXT NOT NULL,
+    first_name TEXT UNIQUE NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
     );`
 	if _, err := db.Exec(query); err != nil {
 		return err
 	}
-	fmt.Println("[SUCCESS]: Created the users table")
+	fmt.Println(": Created the users table")
 	return nil
 }
