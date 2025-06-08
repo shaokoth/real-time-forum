58d4f53e0d10140b1e0ef427480378f0ce94e99a
hilaromondi <omondihilary19@gmail.com>
2025-06-08 06:46:48 +0300

    fix(comments-likes):  delete  id column   and change unique to Primary Key

--- backend/database/commentlikes.go
+++ backend/database/commentlikes.go
@@ -10,12 +10,11 @@ func CreateCommentLikes(db *sql.DB) error {
 		return fmt.Errorf("nil database connection")
 	}
 	query := `CREATE TABLE IF NOT EXISTS comment_likes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER NOT NULL,
     comment_id INTEGER NOT NULL,
     is_like BOOLEAN NOT NULL, -- true for like, false for dislike
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE (user_id, comment_id),
+    PRIMARY KEY (user_id, comment_id),
     FOREIGN KEY (user_id) REFERENCES users (id),
     FOREIGN KEY (comment_id) REFERENCES comments (comment_id)
 );`
