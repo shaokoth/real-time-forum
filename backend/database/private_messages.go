04c3a2f9f6cacf81ad10ce292b4b4746acb3c6d3
hilaromondi <omondihilary19@gmail.com>
2025-06-18 06:31:06 +0300

    fix: CreatePrivateMessagesTable

--- backend/database/private_messages.go
+++ backend/database/private_messages.go
@@ -15,7 +15,6 @@ func CreatePrivateMessagesTable(db *sql.DB) error {
 	CREATE TABLE IF NOT EXISTS private_messages (
 	private_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	content TEXT NOT NULL,
-	nickname TEXT NOT NULL,
     sender_id INTEGER NOT NULL,
     receiver_id INTEGER NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
